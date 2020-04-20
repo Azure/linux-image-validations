547139c8f86d1de8b850d71ffa95dda66d2849d9
sribs <sribs@microsoft.com>
2020-04-20 17:40:41 +0530

    Terraform planning and jsonencode compatible format

--- azure/vm-extension-deploy.tf
+++ azure/vm-extension-deploy.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_machine_extension" "gen1_extension" {
-    name                 = "hostname"
+    name                 = azurerm_linux_virtual_machine.vm_gen1.computer_name
     virtual_machine_id   = azurerm_linux_virtual_machine.vm_gen1.id
     publisher            = "Microsoft.Azure.Extensions"
     type                 = "CustomScript"
@@ -7,7 +7,7 @@ resource "azurerm_virtual_machine_extension" "gen1_extension" {
 
     settings = <<SETTINGS
         {
-            "fileUris": ${jsonencode(var.file_uris)},
+            "fileUris": ${jsonencode("${split(" ", var.file_uris)}")},
             "commandToExecute": "./validate_upload.sh ${var.vhd_name}.vhd gen1"
         }
     SETTINGS
@@ -19,7 +19,7 @@ resource "azurerm_virtual_machine_extension" "gen1_extension" {
 }
 
 resource "azurerm_virtual_machine_extension" "gen2_extension" {
-    name                 = "hostname"
+    name                 = azurerm_linux_virtual_machine.vm_gen2.computer_name
     virtual_machine_id   = azurerm_linux_virtual_machine.vm_gen2.id
     publisher            = "Microsoft.Azure.Extensions"
     type                 = "CustomScript"
@@ -27,7 +27,7 @@ resource "azurerm_virtual_machine_extension" "gen2_extension" {
 
     settings = <<SETTINGS
         {
-            "fileUris": ${jsonencode(var.file_uris)},
+            "fileUris": ${jsonencode("${split(" ", var.file_uris)}")},
             "commandToExecute": "./validate_upload.sh ${var.vhd_name}.vhd gen2"
         }
     SETTINGS
