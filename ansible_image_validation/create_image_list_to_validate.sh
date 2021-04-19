3efe2fcc4f5fcb0ca30e580625371888b3914643
anujmaurya <anujmaurya@microsoft.com>
2021-04-19 12:17:22 +0000

    search for rhel repo ignoring cases and removed sig rhel repos

--- ansible_image_validation/create_image_list_to_validate.sh
+++ ansible_image_validation/create_image_list_to_validate.sh
@@ -1,1 +1,1 @@
-az vm image list --all --publisher redhat | grep urn | cut -d'"' -f4 | grep ':rhel' | grep -v ":6" | grep -v "byos" > files/allimages
+az vm image list --all --publisher redhat | grep urn | cut -d'"' -f4 | grep -i ':rhel' | grep -v ":6" | grep -v "byos" | grep -v "sig"> files/allimages
